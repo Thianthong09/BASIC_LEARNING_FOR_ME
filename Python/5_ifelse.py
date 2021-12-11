6224a6636a291a6f1471da0187d5ed4bfb7a5163
Thian <Thianthong0904@gmail.com>
2021-12-11 17:05:50 +0700

    F

---
+++ Python/5_ifelse.py
@@ -0,0 +1,18 @@
+#Control Structures
+# Sequential    ແບບລຳດັບ
+# Selection     ແບບຄັດເລືອກ
+# Repetition    ການທຳຊ້ຳ
+
+
+# if expression:
+#     statement
+
+
+# 15 - 20 > ໄວໜຸ່ມ
+# 21 - 39 > ໄວທຳງານ
+# 30 - 39 > ໄວຜູ້ໃຫຍ່
+age=int(input("ປ້ອນອາຍຸຂອງທ່ານ: "))
+if age>=15 and age<=20 :
+    print("ໄວໜຸ່ມ") 
+else:
+    print("ໄວເດັກ")
---
+++ Python/Example.py
@@ -0,0 +1,8 @@
+My_string="python"
+x=0
+for i in My_string:
+    x=x+1
+    print(My_string[0:x])
+for i in My_string:
+    x=x-1
+    print(My_string[0:x])

e0f186f380dfc4502300f5b66cb2eb63dc7a0a4e
Thian <Thianthong0904@gmail.com>
2021-12-26 20:59:13 +0700

    F

---
+++ JavaScript/Javascript2/script.js
@@ -0,0 +1,70 @@
+/*ສະແດງແບບ console
+console.log("ສະບາຍດີ");
+document ສະແດງແບບ ຂໍ້ຄວາມ \ ເຂົ້າເຖິງໄອດີ innerHTML set ຄ່າ
+document.getElementById("text").innerHTML = "Thianthong"
+document.write("Hello Document write");
+
+ສະແດງເປັນແບບແຈ້ງເຕືອນ Box
+window.alert("Hello Alert Box");
+*/
+
+// _________________________________
+// Variables In java
+// var x = 100;
+// var y = 100;
+// var z = x + y;
+
+// console.log(z);
+//_____________________________________
+// ການເຊື່ອມຕໍ່ String int 
+// var FirstName = "Thianthong";
+// var LastName = "ONCHAN";
+// var FullName = FirstName +" "+LastName;
+
+// console.log(FullName);
+// ____________________________________
+
+// Operators
+
+// Operators ຄືເຄື່ອງໝາຍຕ່າງໆເອົາໄວ້ໃຊ້ຄຳນວນຫາຄ່າທາງຄະນິດສາດ, ຕະກະສາດ
+
+// number 
+// string 
+// boolean true false 
+
+// Arithmetic Operators
+
+// + ບວກ console.log(5+5); 10
+// - ລົບ console.log(4-2); 2
+// * ຄູນ console.log(5*2); 10
+// / ຫານ console.log(20/2); 10
+// ** ຍົກກຳລັງ console.log(4 ** 2); 16
+// % ຫານເອົາເສດ console.log(10 % 3); 1 
+
+// ++ +1 
+// x++
+// x--
+// ____________________________________
+// comparison operators
+
+// == ===
+// != !==
+// > ໃຫຍ່ກວ່າ
+// >=
+// < ນ້ອຍກວ່າ
+// <=
+// ____________________________________
+// Assignment operators
+
+// x = y 
+// x += y
+// x -= y
+// x *= y
+// x /= y
+//let a = 10;
+//10 += 5;
+
+// ____________________________________
+// Logical operators
+// &&
+// ||
